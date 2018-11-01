4febeb064626025fe28ee558124723f449d94204
pengzg <869279160@qq.com>
2018-11-01 16:47:54 +0800

    modify

--- js/base.js
+++ js/base.js
@@ -21,10 +21,43 @@ $(function(){
         ,autoplay:2000
         ,autoplayDisableOnInteraction:false
         //循环
+        , loop:true       
+      });
+
+      $('#imgSwiper1').swiper({
+        //分页器
+        pagination:'#imgSwiper1 .swiper-pagination'
+        ,paginationClickable:true
+        //自动播放
+        ,autoplay:2000
+        ,autoplayDisableOnInteraction:false
+        //循环
         , loop:true
        
       });
-
+      
+      $('#imgSwiper2').swiper({
+        //分页器
+        pagination:'#imgSwiper2 .swiper-pagination'
+        ,paginationClickable:true
+        //自动播放
+        ,autoplay:2000
+        ,autoplayDisableOnInteraction:false
+        //循环
+        , loop:true
+       
+      });
+      $('#imgSwiper4').swiper({
+        //分页器
+        pagination:'#imgSwiper4 .swiper-pagination'
+        ,paginationClickable:true
+        //自动播放
+        ,autoplay:2000
+        ,autoplayDisableOnInteraction:false
+        //循环
+        , loop:true
+       
+      });
       $('#aboutSwiper').swiper({
         //分页器
         pagination:'#aboutSwiper .about-swiper-pagination'
