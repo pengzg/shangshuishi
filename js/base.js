9c222e3449a1ac622769ae3bfccc6029513d2ac3
pengzg <869279160@qq.com>
2018-11-09 14:39:02 +0800

    人才啊

--- js/base.js
+++ js/base.js
@@ -13,40 +13,6 @@ $(function(){
        
       });
 
-      /* $('#imgSwiper').swiper({
-        //分页器
-        pagination:'#imgSwiper .swiper-pagination'
-        ,paginationClickable:true
-        //自动播放
-        ,autoplay:2000
-        ,autoplayDisableOnInteraction:false
-        //循环
-        , loop:true       
-      });
- */
-      $('#imgSwiper1').swiper({
-        //分页器
-        pagination:'#imgSwiper1 .swiper-pagination'
-        ,paginationClickable:true
-        //自动播放
-        ,autoplay:2000
-        ,autoplayDisableOnInteraction:false
-        //循环
-        , loop:true
-       
-      });
-      
-      $('#imgSwiper2').swiper({
-        //分页器
-        pagination:'#imgSwiper2 .swiper-pagination'
-        ,paginationClickable:true
-        //自动播放
-        ,autoplay:2000
-        ,autoplayDisableOnInteraction:false
-        //循环
-        , loop:true
-       
-      });
       $('#imgSwiper4').swiper({
         //分页器
         pagination:'#imgSwiper4 .swiper-pagination'
@@ -58,18 +24,6 @@ $(function(){
         , loop:true
        
       });
-      $('#aboutSwiper').swiper({
-        //分页器
-        pagination:'#aboutSwiper .about-swiper-pagination'
-        ,paginationClickable:true
-        //自动播放
-        ,autoplay:2000
-        ,autoplayDisableOnInteraction:false
-        //循环
-        , loop:true
-        //左右按钮
-        ,prevButton:'.about-swiper-button-prev'
-        ,nextButton:'.about-swiper-button-next'
-      });
+     
 
 });
